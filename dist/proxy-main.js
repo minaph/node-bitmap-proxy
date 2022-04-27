53782206141afc29370a477a908084f35ae439da
Glitch (exuberant-quilt-chimpanzee) <none>
2022-04-27 03:29:10 +0000

    🍀🏋 Checkpoint

--- dist/proxy-main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BitmapProxy_1 = require("./BitmapProxy");
-(function main() {
-    const proxy = new BitmapProxy_1.BitmapProxy({
-        whiteList: ["https://scrapbox.io"]
-    });
-    proxy.start();
-})();
